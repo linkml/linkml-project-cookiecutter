fa08b42d2a1ddb65a6ffec8fd0a401b2b890f0e9
Harshad Hegde <hegdehb@gmail.com>
2022-09-06 16:06:55 -0500

    testing regex_replace

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -5,11 +5,6 @@ import sys
 from pathlib import Path
 
 MODULE_REGEX = re.compile(r'^[_a-zA-Z][_a-zA-Z0-9]+$')
-HOME_DIR = Path(__file__).parent.parent.resolve()
-EXTENSION_DIR = HOME_DIR / 'extensions'
-
-sys.path.append(str(EXTENSION_DIR))
-
 project_name = '{{ cookiecutter.project_name}}'
 
 if not MODULE_REGEX.match(project_name):
