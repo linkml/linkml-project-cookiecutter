e0dc01079abb980a406ba79e10c0a93fa63131f8
Harshad Hegde <hegdehb@gmail.com>
2022-09-15 11:40:45 -0500

    test jinja if condition

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -9,9 +9,14 @@ shutil.rmtree("licenses")
 
 project_slug = '{{ cookiecutter.__project_slug}}'
 create_python_classes = '{{ cookiecutter.create_python_classes}}'
+use_schemasheets_option = '{{ cookiecutter.use_schemasheets }}'
 
 if create_python_classes == "No":
     print("TODO - cleanup python")
+
+if use_schemasheets_option == "No":
+    print("TODO - Change MakeFile: remove compile-sheets from gen-project")
+
 
 print("** PROJECT CREATION COMPLETE **\n")
 print("Next steps:")
