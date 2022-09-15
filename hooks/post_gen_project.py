6ea87c45fe6c251f5bbe921c2339ce2cc37f05fb
Harshad Hegde <hegdehb@gmail.com>
2022-09-15 15:18:49 -0500

    removed variable assignment

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,14 +7,11 @@ from pathlib import Path
 
 shutil.rmtree("licenses")
 
-project_slug = "{{ cookiecutter.__project_slug}}"
-create_python_classes = "{{ cookiecutter.create_python_classes }}"
-
-if create_python_classes == "No":
+if '{{ cookiecutter.create_python_classes }}' == "No":
     print("TODO - cleanup python")
 
 print("** PROJECT CREATION COMPLETE **\n")
-print(f"** {project_slug} **\n")
+print(f"** {'{{ cookiecutter.__project_slug}}'} **\n")
 print("Next steps:")
 print("cd {{cookiecutter.project_name}}")
 print("make setup")
