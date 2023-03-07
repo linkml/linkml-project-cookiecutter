a5ad962fbad24c86894f7a89df6cc44e968c63a9
Trevor Keller <trevor.keller@nist.gov>
2023-03-07 17:37:27 -0500

    Fix Flake8 linting errors.

--- {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/datamodel/{{cookiecutter.__project_slug}}.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/datamodel/{{cookiecutter.__project_slug}}.py
@@ -7,22 +7,19 @@
 # license: https://creativecommons.org/publicdomain/zero/1.0/
 
 import dataclasses
-import sys
 import re
-from jsonasobj2 import JsonObj, as_dict
+from jsonasobj2 import as_dict
 from typing import Optional, List, Union, Dict, ClassVar, Any
 from dataclasses import dataclass
-from linkml_runtime.linkml_model.meta import EnumDefinition, PermissibleValue, PvFormulaOptions
+from linkml_runtime.linkml_model.meta import EnumDefinition, PermissibleValue
 
 from linkml_runtime.utils.slot import Slot
-from linkml_runtime.utils.metamodelcore import empty_list, empty_dict, bnode
-from linkml_runtime.utils.yamlutils import YAMLRoot, extended_str, extended_float, extended_int
+from linkml_runtime.utils.metamodelcore import empty_list, empty_dict
+from linkml_runtime.utils.yamlutils import YAMLRoot, extended_str
 from linkml_runtime.utils.dataclass_extensions_376 import dataclasses_init_fn_with_kwargs
-from linkml_runtime.utils.formatutils import camelcase, underscore, sfx
 from linkml_runtime.utils.enumerations import EnumDefinitionImpl
-from rdflib import Namespace, URIRef
+from rdflib import URIRef
 from linkml_runtime.utils.curienamespace import CurieNamespace
-from linkml_runtime.linkml_model.types import Boolean, Date, Integer, String
 from linkml_runtime.utils.metamodelcore import Bool, XSDDate
 
 metamodel_version = "1.7.0"
@@ -40,7 +37,6 @@ MY_DATAMODEL = CurieNamespace('my_datamodel', 'https://w3id.org/my_org/my_datamo
 PROV = CurieNamespace('prov', 'http://www.w3.org/ns/prov#')
 SCHEMA = CurieNamespace('schema', 'http://schema.org/')
 DEFAULT_ = MY_DATAMODEL
-
 
 # Types
 
