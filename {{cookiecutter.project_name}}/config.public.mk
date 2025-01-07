340158e34343b9bec943e08d7a6b5318f35c6e5b
David Linke <dr.david.linke@gmail.com>
2025-01-08 00:01:06 +0100

    Simplify makefile and config

--- {{cookiecutter.project_name}}/config.public.mk
+++ {{cookiecutter.project_name}}/config.public.mk
@@ -11,13 +11,14 @@ LINKML_SCHEMA_NAME={{cookiecutter.__project_slug}}
 LINKML_SCHEMA_AUTHOR={{cookiecutter.__author}}
 LINKML_SCHEMA_DESCRIPTION={{cookiecutter.project_description}}
 LINKML_SCHEMA_SOURCE_PATH={{cookiecutter.__source_path}}
+LINKML_SCHEMA_GOOGLE_SHEET_MODULE={{cookiecutter.__google_sheet_module}}
 LINKML_SCHEMA_GOOGLE_SHEET_ID={{cookiecutter.google_sheet_id}}
 LINKML_SCHEMA_GOOGLE_SHEET_TABS={{cookiecutter.google_sheet_tabs}}
 
 ###### linkml generator variables, used by makefile
 
 ## gen-project configuration file
-LINKML_GENERATORS_CONFIG_YAML= --config-file config.yaml
+LINKML_GENERATORS_CONFIG_YAML=config.yaml
 
 ## pass args if gendoc ignores config.yaml (i.e. --no-mergeimports)
 LINKML_GENERATORS_DOC_ARGS=
@@ -29,4 +30,3 @@ LINKML_GENERATORS_OWL_ARGS=
 ## pass args to trigger experimental java/typescript generation
 LINKML_GENERATORS_JAVA_ARGS=
 LINKML_GENERATORS_TYPESCRIPT_ARGS=
-
