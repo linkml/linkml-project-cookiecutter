239051f22e4bc06a8e4b907adeb323c135c338c7
David Linke <dr.david.linke@gmail.com>
2025-01-11 00:44:04 +0100

    Quote values in dotenv file

--- {{cookiecutter.project_name}}/config.public.mk
+++ {{cookiecutter.project_name}}/config.public.mk
@@ -7,13 +7,13 @@
 ###### schema definition variables, used by makefile
 
 # Note: makefile variables should not be quoted, as makefile handles quoting differently than bash
-LINKML_SCHEMA_NAME={{cookiecutter.__project_slug}}
-LINKML_SCHEMA_AUTHOR={{cookiecutter.__author}}
-LINKML_SCHEMA_DESCRIPTION={{cookiecutter.project_description}}
-LINKML_SCHEMA_SOURCE_PATH={{cookiecutter.__source_path}}
-LINKML_SCHEMA_GOOGLE_SHEET_MODULE={{cookiecutter.__google_sheet_module}}
-LINKML_SCHEMA_GOOGLE_SHEET_ID={{cookiecutter.google_sheet_id}}
-LINKML_SCHEMA_GOOGLE_SHEET_TABS={{cookiecutter.google_sheet_tabs}}
+LINKML_SCHEMA_NAME="{{cookiecutter.__project_slug}}"
+LINKML_SCHEMA_AUTHOR="{{cookiecutter.__author}}"
+LINKML_SCHEMA_DESCRIPTION="{{cookiecutter.project_description}}"
+LINKML_SCHEMA_SOURCE_PATH="{{cookiecutter.__source_path}}"
+LINKML_SCHEMA_GOOGLE_SHEET_MODULE="{{cookiecutter.__google_sheet_module}}"
+LINKML_SCHEMA_GOOGLE_SHEET_ID="{{cookiecutter.google_sheet_id}}"
+LINKML_SCHEMA_GOOGLE_SHEET_TABS="{{cookiecutter.google_sheet_tabs}}"
 
 ###### linkml generator variables, used by makefile
 
