f39735238af69e96f760828a638d9ca9348a6fcb
Manuel Lera-Ramirez <manulera14@gmail.com>
2025-05-22 11:15:41 +0100

    Update _version.py
    At least with my directory structure, `__name__` resolves to `package_name._version`, what you want is `package_name`

--- {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/_version.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/_version.py
@@ -1,7 +1,7 @@
 from importlib.metadata import version, PackageNotFoundError
 
 try:
-    __version__ = version(__name__)
+    __version__ = version(__name__.split(".")[0])
 except PackageNotFoundError:
     # package not installed
     __version__ = "0.0.0"
