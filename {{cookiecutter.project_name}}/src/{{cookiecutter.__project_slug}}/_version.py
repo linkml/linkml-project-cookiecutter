e5f9990b2f4b19e2b1f695f3c4e140d01687803d
Manuel Lera-Ramirez <manulera14@gmail.com>
2025-10-23 22:14:00 +0100

    Fix version assignment logic in _version.py
    Consider the case where the module would be called directly

--- {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/_version.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/_version.py
@@ -1,7 +1,10 @@
 from importlib.metadata import version, PackageNotFoundError
 
-try:
-    __version__ = version(__name__.split(".")[0])
-except PackageNotFoundError:
-    # package not installed
-    __version__ = "0.0.0"
+# Return 0.0.0 if package not installed
+__version__ = "0.0.0"
+
+if __package__:
+    try:
+        __version__ = version(__package__)
+    except PackageNotFoundError:
+        pass
